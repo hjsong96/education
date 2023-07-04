a582a03847f04dde4c3304034e1d39cbc63bba3d
hjsong96 <133767846+hjsong96@users.noreply.github.com>
2023-07-04 18:29:59 +0900

    jul03 update file

---
+++ jul03/src/main/java/com/poseidon/board/Detail.java
@@ -0,0 +1,23 @@
+package com.poseidon.board;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/detail")
+public class Detail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Detail() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//http://localhost:8080/jul03/detail?bno=1
+		System.out.println(request.getParameter(getServletName()));
+	}
+
+}
